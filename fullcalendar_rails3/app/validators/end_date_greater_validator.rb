2a035b39b33778c8901af9b54ca3a24c2a5db2eb
Brett Nisbett <brett.nisbett@thescore.com>
2010-08-25 14:14:37 -0400

    Removed Live Validations Plugin

--- fullcalendar_rails3/app/validators/end_date_greater_validator.rb
+++
@@ -1,7 +0,0 @@
-class EndDateGreaterValidator < ActiveModel::EachValidator
-  def validate_each(object, attribute, value)
-    unless value =~ 
-      object.errors[attribute] << (options[:message] || "is not formatted properly")
-    end
-  end
-end
